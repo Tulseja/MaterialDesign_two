862b5c0c8b3549348b221535c93abadeaf88781f
Nikhil Tulseja <nikhiltulseja1993@gmail.com>
2016-11-15 01:39:20 +0530

    Basic Login Screen

---
+++ MyApplication/app/src/main/java/com/wacky/matdesign/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.wacky.matdesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+//import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        TextView registerScreen = (TextView) findViewById(R.id.link_to_register);
+
+        registerScreen.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // Switching to Register screen
+                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(i);
+            }
+    });
+
+}
+}
